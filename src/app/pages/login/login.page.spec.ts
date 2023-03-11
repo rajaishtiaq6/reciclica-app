a43d96f13ac1993f20948cde6cf63aaaf4da1455
Ishtiaq Ahmed <rajaishtiaq6@gmail.com>
2023-03-11 05:52:20 +0500

    Login form validation

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -4,6 +4,7 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPage } from './login.page';
 import { AppRoutingModule } from '../../app-routing.module';
 import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginPage', () => {
   let component: LoginPage;
@@ -13,33 +14,32 @@ describe('LoginPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginPage ],
-      imports: [IonicModule.forRoot(), AppRoutingModule]
+      imports: [IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
 
-    router = TestBed.get(Router);
+    router = TestBed.inject(Router);
 
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
+  it('should create form on init', ()=> {
+    component.ngOnInit();
+    expect(component.form).not.toBeUndefined();
+  })
+
   it('should go to home page on login', () => {
-
     spyOn(router, 'navigate');
-
     component.login();
-
     expect(router.navigate).toHaveBeenCalledWith(['home']);
 
   });
 
   it('should go to register page on register', () => {
-
     spyOn(router, 'navigate');
-
     component.register();
-
     expect(router.navigate).toHaveBeenCalledWith(['register']);
 
   });
